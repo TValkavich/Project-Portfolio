6c7f2c3585a1e6bceeb4f9ac717b2e77aab8204a
TValk <tjvalkav@buffalo.edu>
2021-04-08 19:25:42 -0400

    Playing around with typewriter effect

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import Typewriter from 'typewriter-effect'
+import styled from 'styled-components';
+
+const TypewriterStyle = styled.div`
+    color: #22e0f5;
+    font-size: 70px;
+    display: inline-flex;
+    padding-top:10%
+`;
 
 export default function Home() {
     return (
-        <div>
-            <Typewriter
-                options={{
-                strings: "Tyler Valkavich",
-                autoStart: true,
-                loop: false,
-            }}
-            />
-        </div>
-
-        
+        <React.Fragment>
+            <TypewriterStyle>
+                &gt;
+                <Typewriter
+                    options={{
+                    strings: 'Tyler Valkavich',
+                    autoStart: true,
+                    loop: false,
+                }}
+                />
+            </TypewriterStyle>
+        </React.Fragment>
     )
 }
+
+

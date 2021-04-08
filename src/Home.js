5e31a34c5de757f0590f1a309b20fc674d624e7b
TValk <tjvalkav@buffalo.edu>
2021-04-08 18:42:47 -0400

    Playing around with some components. Thinking about going with a static page for this one.

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Typewriter from 'typewriter-effect'
 
 export default function Home() {
     return (
         <div>
-            <h1>This is the Home/Splash Page </h1>
-            <p>Wooohooooooooooooo we're at the splash page</p>
+            <Typewriter
+                options={{
+                strings: "Tyler Valkavich",
+                autoStart: true,
+                loop: false,
+            }}
+            />
         </div>
+
+        
     )
 }
